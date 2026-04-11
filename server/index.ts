6b5040c864bcc6a32c581854abcd4dd143967c39
Oriol Sastre Rienitz <oriol.sastre@gmail.com>
2026-04-11 14:44:10 +0200

    Remove SSR

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import ViteExpress from "vite-express";
+import path from "path";
+import { fileURLToPath } from "url";
 import apiRouter from "./routes/router.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
@@ -9,6 +12,12 @@ app.use(cookieParser());
 
 app.use("/api", apiRouter);
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server running at http://localhost:3000")
-);
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get(".{0,}", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
